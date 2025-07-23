542035bcc915b6be1278570b4d8bf0cf4c0353ee
Kadri <kadricabuk@gmail.com>
2025-07-23 23:44:31 +0300

    Update app build and server manifests, enhance image optimization settings, and refine error handling for improved user experience. Adjusted Next.js configuration for better performance and responsiveness across various components.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ExHkLR1gysQ2x7UY8PR4GlyRc8+JyJMtJZ8ZklQTiwo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
