90ed56fbe2598bfaddee01fa5d16915c6f9870fa
Kadri <kadricabuk@gmail.com>
2025-08-16 13:30:31 +0300

    Refactor CarouselSection component to replace button elements with divs for improved layout and accessibility. Adjust onClick handling for Carousel to ensure proper interaction while maintaining visual structure.

--- src/app/layout/CarouselSection.tsx
+++ src/app/layout/CarouselSection.tsx
@@ -4,17 +4,17 @@ const CarouselSection = ({ carouselPlacement, data, children, className, onClick
     return (
         <div className={`bg-white items-center flex flex-col-reverse md:flex-row py-8  ${className}`}>
             {carouselPlacement === "left" &&
-                <button onClick={() => onClick?.(0)} className="w-full md:w-1/2 h-full flex flex-col justify-center items-center cursor-pointer">
+                <div className="w-full md:w-1/2 h-full flex flex-col justify-center items-center cursor-pointer">
                     <Carousel slides={data} />
-                </button>
+                </div>
             }
             <div className="w-full md:w-1/2 flex flex-col justify-center items-center gap-[12px] h-full p-4 md:p-0">
                 {children}
             </div>
             {carouselPlacement === "right" &&
-                <button onClick={() => onClick?.(0)} className="w-full md:w-1/2 h-full flex flex-col justify-center items-center cursor-pointer">
-                    <Carousel slides={data} />
-                </button>
+                <div className="w-full md:w-1/2 h-full flex flex-col justify-center items-center cursor-pointer">
+                    <Carousel slides={data} onClick={onClick} />
+                </div>
             }
         </div>
     )
